40e2c7fbf72bdfaccf10a04a3c90644e94b8b6e8
roy-boy <rocrr.li@gmail.com>
2017-10-27 11:29:26 +1100

    Update handy_methods.py
    adding a new compare dates method

--- handy_methods.py
+++ handy_methods.py
@@ -7,3 +7,24 @@ def second_largest(test_list):
             first, second = n, first
     # return first
     return second
+
+
+def compare_dates(utc_date, local_date):
+    """take two input date format and convert gwml_date(local) to UTC
+    :param utc_date
+    :param local_date
+    :return:
+        a string passed or failed
+    """
+    utc_tz = tz.gettz('UTC')  # get the timezone
+    fix_date_new = parse(utc_date)
+    fix_date_time = fix_date_new.strftime('%Y-%m-%d %H:%M:%S')  # format the time stamp
+    gwml_date_new = parse(local_date)
+    gwml_date_utc = gwml_date_new.astimezone(utc_tz)  # convert to UTC time
+    gwml_date_time = gwml_date_utc.strftime('%Y-%m-%d %H:%M:%S')  # format the time stamp
+    if fix_date_time == gwml_date_time:
+        log_test_output('Date matches.')
+        return 'passed'
+    else:
+        log_test_output('tag value diff in FIX: ' + fix_date_time + ' <---> value in GWML: ' + gwml_date_time)
+        return 'failed'
